d3db08a63ea6da6ecd757f5cbf7bd75a6e2cf141
GuyRoberts1972 <guywilliamwelchroberts@gmail.com>
2020-02-23 22:58:45 +0000

    split out short codes into base class

--- src/class.render.php
+++ src/class.render.php
@@ -9,11 +9,50 @@
 // END_NAMESPACE_EDIT_MARKER
 
 // Our plugins render class 
-class Render {
+abstract class Render {
 	
-
-	// Constructor
-	function __construct() {
+	// Class member variabes
+	private $short_code_tag = "";
+	
+	// Get short code tag
+	function get_short_code_tag(){ 
+		return $this->short_code_tag; 
 	}
+	
+	// Abstract methods
+	
+	// Called to render the short code 
+	abstract function render_shortcode();
+	
+	
+	// Constructor
+	function __construct( $short_code_tag ) {
+		
+		// Check params
+		assert(! empty( $short_code_tag ),	'$short_code_tag cannot be empty.');
+			      
+		// Assign to class 
+		$this->short_code_tag = $short_code_tag;
+		
+		// Register for init call back
+    	add_action( 'init', array ($this, 'fn_register_short_codes'));
+	}
+	
+	// Call back to register short codes. 
+	function fn_register_short_codes() {
+	  
+	    // Add the short code(s)
+	    add_shortcode($this->short_code_tag, array ($this, 'fn_render_shortcode'));
+	    
+	}
+	
+	// Call back to render the short code 
+	function fn_render_shortcode(){
+		
+		// Call child class 
+		return $this->render_shortcode();
+	}
+ 
+ 
 	
 }
--- tests/test-class.render.php
+++ tests/test-class.render.php
@@ -4,17 +4,60 @@
 * Test Utilities
 */
 require_once dirname( __FILE__ ) . "/../src/class.render.php";
+require_once dirname( __FILE__ ) . "/../src/utils.php";
 
-class RenderTest extends WP_UnitTestCase
+
+class Render_SubClass extends Render {
+    
+    static public $test_render_html = "<div> <h1> test heading  </h1> </div>";
+    
+    // Instanciate the abstract method
+    function render_shortcode(){
+        return self::$test_render_html ;
+    }
+    
+    // Static member to access to private methods for testing
+    static function test_methods_privately($test_case){
+        
+        // Do the init
+        $short_code_tag = 'short_code';
+        ob_start();
+        $renderer = new Render_SubClass($short_code_tag);
+        $renderer->fn_register_short_codes();
+        $output = ob_get_contents();
+        $test_case->assertTrue("" == $output);
+        ob_end_clean();
+        
+        // Check at least one 
+        global $shortcode_tags;
+        $test_case->assertTrue( 1 <= count($shortcode_tags));
+        
+        // Check it's there 
+        $test_case->assertTrue(isset($shortcode_tags[$short_code_tag]));
+        
+        // Check short code getter
+        $test_case->assertTrue($short_code_tag === $renderer->get_short_code_tag());
+        
+        // Call render
+        $test_case->assertTrue(is_valid_html($renderer->fn_render_shortcode()));
+        $test_case->assertTrue(is_valid_html(self::$test_render_html === $renderer->fn_render_shortcode()));
+
+    }
+
+}
+
+class Render_Test extends WP_UnitTestCase
 {
 
 
 
-    public function test_dbg_trace()
+    public function test_methods()
     {
         $this->assertTrue( true);
         
-        $render_class = new Render();
+        Render_SubClass::test_methods_privately($this);
+        
+        
     }
     
 }
